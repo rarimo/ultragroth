764fa862b6ddb3f6b52c644b557b8d601f616044
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2025-11-25 15:39:36 +0200

    hotfix: set return 0 instead of return 1 for the build_gmp.sh script

--- build_gmp.sh
+++ build_gmp.sh
@@ -49,7 +49,7 @@ build_aarch64()
 
     if [ -d "$PACKAGE_DIR" ]; then
         echo "aarch64 package is built already. See $PACKAGE_DIR"
-        return 1
+        return 0
     fi
 
 
@@ -75,7 +75,7 @@ build_host()
 
     if [ -d "$PACKAGE_DIR" ]; then
         echo "Host package is built already. See $PACKAGE_DIR"
-        return 1
+        return 0
     fi
 
     rm -rf "$BUILD_DIR"
@@ -96,7 +96,7 @@ build_host_noasm()
 
     if [ -d "$PACKAGE_DIR" ]; then
         echo "Host package is built already. See $PACKAGE_DIR"
-        return 1
+        return 0
     fi
 
     rm -rf "$BUILD_DIR"
@@ -117,7 +117,7 @@ build_android()
 
     if [ -d "$PACKAGE_DIR" ]; then
         echo "Android package is built already. See $PACKAGE_DIR"
-        return 1
+        return 0
     fi
 
     if [ -z "$ANDROID_NDK" ]; then
@@ -166,7 +166,7 @@ build_android_x86_64()
 
     if [ -d "$PACKAGE_DIR" ]; then
         echo "Android package is built already. See $PACKAGE_DIR"
-        return 1
+        return 0
     fi
 
     if [ -z "$ANDROID_NDK" ]; then
@@ -215,7 +215,7 @@ build_ios()
 
     if [ -d "$PACKAGE_DIR" ]; then
         echo "iOS package is built already. See $PACKAGE_DIR"
-        return 1
+        return 0
     fi
 
     export SDK="iphoneos"
